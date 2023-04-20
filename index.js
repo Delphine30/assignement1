aaf5f13e78eb2cdc2e869e58a666ef0aa9a41c71
Delphine30 <kamusabwadelphine@gmail.com>
2023-04-20 13:13:51 +0200

    new

---
+++ index.js
@@ -0,0 +1,23 @@
+// function learnmore(){
+// document.querySelector("#middle").style.disploy="block";
+// }
+function lineAppear(){
+    document.getElementById("umurongo").style.textDecoration="underline"
+    document.getElementById("umurongo").style.textDecorationColor="aqua"
+}
+function lineDisappear(){
+    document.getElementById("umurongo").style.textDecoration="none"
+}
+function lineOn(){
+    document.getElementById("right").style.textDecoration="underline"
+    document.getElementById("right").style.textDecorationColor="aqua"
+}
+function lineOut(){
+    document.getElementById("right").textDecoration="none"
+}
+function changeColor(){
+    document.getElementById("btn").style.backgroundColor="green"
+}
+function displayItems(){
+    document.querySelector(".container").style.display="block"
+}
